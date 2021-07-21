01a17777dabbf5b1679127d6a75a604b287d10ff
root <root@ip-172-31-6-236.ec2.internal>
2021-07-21 20:57:15 +0000

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  app.get("/filteredimage/", async (req, res) =>{
+  app.get('/filteredimage', async (req:express.Request, res:express.Response) => {
     try{
       let {image_url} = req.query;
       if(!image_url){
